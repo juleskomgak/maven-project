7b7f58e20cc60a36e6fbe89ad8c843bea1f9989f
KOMGA Jules <jules.komga-extern@renault.com>
2018-06-16 21:38:07 +0200

    formatting greeting class 31

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,8 @@ public class Greeter {
 } 
 /**
    * This is a constructor.
-   * @ param string name
-   * @ return text
+   * @param string name
+   * @return text
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
